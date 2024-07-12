d6b56acb8fc9cf9274aff776363e04cf86da3ddc
Gustavo Barrera <gustavo.8805@gmail.com>
2024-07-12 09:31:51 -0600

    creación proyecto

---
+++ src/hello_world/index.js
@@ -0,0 +1,10 @@
+// En Node.js para importar un módulo necesitas de la palabra reservada require
+require('http');
+
+console.log('Hello world!');
+
+function sum (num1, num2) {
+    return num1 + num2;
+}
+
+console.log('La suma es: ', sum(10, 30));
